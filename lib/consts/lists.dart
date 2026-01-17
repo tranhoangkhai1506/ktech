16c90da53a427f27443569ee0f81ee393af68680
KaaKa <you@example.com>
2026-01-17 23:00:58 +0700

    add item detail screen

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -1,11 +1,7 @@
 import 'package:ktechshop/consts/images.dart';
 import 'package:ktechshop/consts/strings.dart';
 
-const socialIconList = [
-  icFacebookLogo,
-  icGoogleLogo,
-  icTwitterLogo
-];
+const socialIconList = [icFacebookLogo, icGoogleLogo, icTwitterLogo];
 
 const sliderList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
 const secondSliderList = [imgSs1, imgSs2, imgSs3, imgSs4];
@@ -15,5 +11,34 @@ const featuredImages2 = [imgS4, imgS5, imgS6];
 const featuredTitles1 = [womenDress, girlsWatch];
 const featuredTitles2 = [boysGlass, mobiphone];
 
-const cateList = [womenClothing, menClothing, kidstoy, phones, fashion, skincare, cars, laptops, suit];
-const cateImages = [imgFc1, imgFc2, imgFc3, imgFc4, imgFc5, imgFc6, imgFc7,imgFc8, imgFc9];
+const cateList = [
+  womenClothing,
+  menClothing,
+  kidstoy,
+  phones,
+  fashion,
+  skincare,
+  cars,
+  laptops,
+  suit,
+];
+
+const cateImages = [
+  imgFc1,
+  imgFc2,
+  imgFc3,
+  imgFc4,
+  imgFc5,
+  imgFc6,
+  imgFc7,
+  imgFc8,
+  imgFc9,
+];
+
+const itemDetaiButtonsList = [
+  video,
+  reviews,
+  returnPolicy,
+  sellerpolicy,
+  supportPolicy,
+];
--- lib/views/category_screen/item.detal.dart
+++ lib/views/category_screen/item.detal.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ktechshop/consts/consts.dart';
+import 'package:ktechshop/consts/lists.dart';
+import 'package:ktechshop/widgets_common/our_button.dart';
 
 class ItemDetail extends StatelessWidget {
   final String? title;
@@ -19,7 +21,245 @@ class ItemDetail extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    VxSwiper.builder(
+                      autoPlay: true,
+                      height: 350,
+                      itemCount: 3,
+                      aspectRatio: 16 / 9,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          imgFc2,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+
+                    10.heightBox,
+                    title!.text
+                        .size(16)
+                        .color(darkFontGrey)
+                        .fontFamily(bold)
+                        .make(),
+
+                    10.heightBox,
+                    VxRating(
+                      onRatingUpdate: (value) {},
+                      normalColor: textfieldGrey,
+                      selectionColor: golden,
+                      count: 5,
+                      size: 25,
+                      stepInt: true,
+                    ),
+
+                    10.heightBox,
+                    "\$30.000".text
+                        .size(16)
+                        .color(darkFontGrey)
+                        .fontFamily(bold)
+                        .make(),
+
+                    10.heightBox,
+                    Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  "Seller".text
+                                      .size(16)
+                                      .fontFamily(semibold)
+                                      .make(),
+                                  5.heightBox,
+                                  "In House Brand".text
+                                      .size(16)
+                                      .fontFamily(semibold)
+                                      .make(),
+                                ],
+                              ),
+                            ),
+                            const CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Icon(
+                                Icons.message_rounded,
+                                color: darkFontGrey,
+                              ),
+                            ),
+                          ],
+                        ).box
+                        .height(60)
+                        .padding(const EdgeInsets.symmetric(horizontal: 16))
+                        .color(textfieldGrey)
+                        .make(),
+
+                    10.heightBox,
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: "Color: ".text.color(textfieldGrey).make(),
+                            ),
+                            Row(
+                              children: List.generate(
+                                3,
+                                (index) => VxBox()
+                                    .size(40, 40)
+                                    .roundedFull
+                                    .color(Vx.randomPrimaryColor)
+                                    .margin(
+                                      const EdgeInsets.symmetric(horizontal: 4),
+                                    )
+                                    .make(),
+                              ),
+                            ),
+                          ],
+                        ).box.padding(const EdgeInsets.all(8)).make(),
+
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: "Quantity: ".text
+                                  .color(textfieldGrey)
+                                  .make(),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.remove),
+                                ),
+                                "0".text.size(16).color(darkFontGrey).make(),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.add),
+                                ),
+                                5.widthBox,
+                                "0 available".text
+                                    .size(16)
+                                    .color(textfieldGrey)
+                                    .make(),
+                              ],
+                            ),
+                          ],
+                        ).box.padding(const EdgeInsets.all(8)).make(),
+
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: "Total: ".text.color(textfieldGrey).make(),
+                            ),
+                            "\$0.00".text
+                                .size(16)
+                                .color(redColor)
+                                .fontFamily(bold)
+                                .make(),
+                          ],
+                        ).box.padding(const EdgeInsets.all(8)).make(),
+                      ],
+                    ).box.white.shadowSm.make(),
+
+                    10.heightBox,
+                    "Description".text
+                        .size(16)
+                        .color(darkFontGrey)
+                        .fontFamily(semibold)
+                        .make(),
+                    10.heightBox,
+                    "This is a description of the product".text
+                        .size(16)
+                        .color(darkFontGrey)
+                        .fontFamily(semibold)
+                        .make(),
+
+                    10.heightBox,
+
+                    ListView(
+                      shrinkWrap: true,
+                      children: List.generate(
+                        itemDetaiButtonsList.length,
+                        (index) => ListTile(
+                          title: itemDetaiButtonsList[index].text
+                              .fontFamily(semibold)
+                              .color(darkFontGrey)
+                              .make(),
+                          trailing: const Icon((Icons.arrow_forward)),
+                        ),
+                      ),
+                    ),
+
+                    20.heightBox,
+                    "Products may you like".text
+                        .size(16)
+                        .color(darkFontGrey)
+                        .fontFamily(semibold)
+                        .make(),
+                    10.heightBox,
+
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(
+                          6,
+                          (index) =>
+                              Column(
+                                    children: [
+                                      Image.asset(
+                                        imgP1,
+                                        width: 150,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      10.heightBox,
+                                      "Laptop 4GB/64GB".text
+                                          .fontFamily(semibold)
+                                          .color(darkFontGrey)
+                                          .make(),
+                                      "\$599".text
+                                          .fontFamily(semibold)
+                                          .color(redColor)
+                                          .size(16)
+                                          .make(),
+                                    ],
+                                  ).box.white
+                                  .margin(
+                                    const EdgeInsets.symmetric(horizontal: 6),
+                                  )
+                                  .rounded
+                                  .padding(const EdgeInsets.all(8))
+                                  .make(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 60,
+            child: ourButton(
+              color: redColor,
+              onPress: () {},
+              textColor: whiteColor,
+              title: "Add to Cart",
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
